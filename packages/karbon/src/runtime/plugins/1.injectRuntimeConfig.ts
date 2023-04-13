7c0240857003ed061469357dd574a15018d15aae
Sid <sid@storipress.com>
2023-04-13 08:48:29 +0800

    fix: inject storipressConfig to storipressConfigCtx

---
+++ packages/karbon/src/runtime/plugins/1.injectRuntimeConfig.ts
@@ -0,0 +1,12 @@
+import { storipressConfigCtx } from '../composables/storipress-base-client'
+import { defineNuxtPlugin, useRuntimeConfig } from '#imports'
+
+export default defineNuxtPlugin((_nuxtApp) => {
+  const runtimeConfig = useRuntimeConfig()
+  storipressConfigCtx.set(runtimeConfig.storipress)
+  return {
+    provide: {
+      runtimeConfig,
+    },
+  }
+})
