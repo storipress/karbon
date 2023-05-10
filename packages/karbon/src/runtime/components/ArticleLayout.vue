f83aad3a95150f978d863848804320088ae73f96
Sid <sid@storipress.com>
2023-05-10 09:26:16 +0800

    feat: Provide NewsArticle meta as JSON-LD

--- packages/karbon/src/runtime/components/ArticleLayout.vue
+++ packages/karbon/src/runtime/components/ArticleLayout.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { parse } from 'node-html-parser'
 import { getDefaultLayout } from '../utils/default-layout'
 import type { UseArticleReturn as Article } from '../types'
 import { useProvideArticle } from '../article/utils'
@@ -25,6 +26,51 @@ function resolveLayout(): string {
 }
 
 useProvideArticle(props.article)
+
+const pageMeta = useResourcePageMeta()
+if (pageMeta.value) {
+  useSchemaOrg([getDefineArticle(pageMeta.value)])
+}
+type PageMeta = NonNullable<(typeof pageMeta)['value']>
+
+const site = useSite()
+function getDefineArticle(pageMeta: PageMeta) {
+  const article: Article = pageMeta.meta
+  const authors = article.authors.map((author) => {
+    const { first_name, last_name, full_name, id } = author
+    return {
+      '@type': 'Person',
+      familyName: last_name,
+      givenName: first_name,
+      name: full_name,
+      mainEntityOfPage: `author/${id}`,
+    }
+  })
+  const doc = parse(article.html)
+  const imgElements = [...doc.querySelectorAll('img')]
+  const imgSrcList = imgElements.map((el) => el.getAttribute('src')).filter(Boolean)
+  const image = [...new Set([article.cover.url, ...imgSrcList])]
+
+  return defineArticle({
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    url: pageMeta.route,
+    publisher: {
+      '@type': 'Organization',
+      name: site.value.name || '',
+      logo: site.value.logo,
+    },
+    headline: article.title,
+    mainEntityOfPage: {
+      '@type': 'Article',
+      '@id': pageMeta.route,
+    },
+    articleBody: article.plaintext,
+    authors,
+    image,
+    datePublished: article.published_at,
+  })
+}
 </script>
 
 <template>
