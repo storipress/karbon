3da264b4e5c6128e7b7b15da7fefbe1af13f158e
ches4117 <ches4117@gmail.com>
2023-06-14 17:24:05 +0800

    fix: filt non published article

--- packages/karbon/src/runtime/routes/atom.xml.ts
+++ packages/karbon/src/runtime/routes/atom.xml.ts
@@ -1,6 +1,6 @@
 import { defineEventHandler, setHeader } from 'h3'
 import { Feed } from 'feed'
-import { encodePath, joinURL } from 'ufo'
+import { encodePath, joinURL, withTrailingSlash } from 'ufo'
 import { listFeedArticles } from '@storipress/karbon/internal'
 import type { Author } from '../composables/page-meta'
 import { useRuntimeConfig } from '#imports'
@@ -28,8 +28,8 @@ export default defineEventHandler(async (e) => {
 
   const siteUrl = runtimeConfig.public.siteUrl
   const feed = new Feed({
-    id: runtimeConfig.public.siteUrl,
-    link: runtimeConfig.public.siteUrl,
+    id: withTrailingSlash(runtimeConfig.public.siteUrl),
+    link: withTrailingSlash(runtimeConfig.public.siteUrl),
     title: runtimeConfig.public.siteName,
     description: runtimeConfig.public.siteDescription,
     updated: new Date(),
@@ -38,18 +38,22 @@ export default defineEventHandler(async (e) => {
     },
   })
 
-  articles.forEach((article: TArticle) => {
-    const id = encodePath(urls.article.toURL(article, urls.article._context))
-    feed.addItem({
-      title: article.title,
-      id,
-      link: joinURL(siteUrl, id),
-      description: article.plaintext.slice(0, 120),
-      date: new Date(article.published_at),
-      author: article.author,
-      content: article.html,
+  articles
+    .filter((article: TArticle) => article.published_at)
+    .forEach((article: TArticle) => {
+      const id = encodePath(urls.article.toURL(article, urls.article._context))
+      feed.addItem({
+        title: article.title,
+        id: joinURL(siteUrl, id),
+        link: joinURL(siteUrl, id),
+        description: article.plaintext.slice(0, 120),
+        date: new Date(article.published_at),
+        author: article.author?.map((author) => ({
+          name: author.name,
+        })) || [{ name: 'Storipress' }],
+        content: article.html,
+      })
     })
-  })
 
   return feed.atom1()
 })
