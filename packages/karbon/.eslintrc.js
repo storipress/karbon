7e4dc06fe181052ce982522d854d7916a230cde3
ches4117 <ches4117@gmail.com>
2023-06-14 17:24:05 +0800

    fix: filt non published article

--- packages/karbon/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@antfu/vue', 'prettier', 'prettier/prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'antfu/if-newline': 'off',
-  },
-}
