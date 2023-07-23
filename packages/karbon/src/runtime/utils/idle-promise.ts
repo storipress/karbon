3e7ec9e846d692f2e9efe41418fef1774439ba60
DanSnow <dododavid006@gmail.com>
2023-07-23 16:10:17 +0800

    fix: improve idle lazy load

--- packages/karbon/src/runtime/utils/idle-promise.ts
+++ packages/karbon/src/runtime/utils/idle-promise.ts
@@ -1,7 +1,32 @@
-export function waitIdle() {
+export async function waitIdle() {
+  await windowLoaded()
+  await idle()
+}
+
+function idle() {
   return new Promise<void>((resolve) => {
     if (typeof requestIdleCallback === 'function') {
-      requestIdleCallback(() => resolve())
+      requestIdleCallback(() => resolve(), { timeout: 1000 })
+    } else {
+      setTimeout(resolve, 300)
+    }
+  })
+}
+
+function windowLoaded() {
+  if (typeof document !== 'undefined' && document.readyState === 'complete') {
+    return Promise.resolve()
+  }
+
+  return new Promise<void>((resolve) => {
+    if (typeof window !== 'undefined' && window.addEventListener) {
+      window.addEventListener(
+        'load',
+        () => {
+          resolve()
+        },
+        { once: true },
+      )
     } else {
       setTimeout(resolve, 300)
     }
