90f6e50a2dac8ec26d6bc31e3eea7488ae68dac7
Sid Chang <37400982+SidStraw@users.noreply.github.com>
2023-09-05 15:53:15 +0800

    fix: [SPMVP-6438] [SPMVP-6321] [SPMVP-6289] article JSON-LD (#238)
    Co-authored-by: DanSnow <dododavid006@gmail.com>

--- packages/karbon/src/runtime/composables/article-schema.ts
+++ packages/karbon/src/runtime/composables/article-schema.ts
@@ -1,33 +1,72 @@
 import { parse } from 'node-html-parser'
+import { resolveURL, withHttps, withoutTrailingSlash } from 'ufo'
+import { Hookable } from 'hookable'
 import type { UseArticleReturn as Article } from '../types'
-import { defineArticle, defineOrganization, definePerson, useResourcePageMeta, useSchemaOrg, useSite } from '#imports'
+import {
+  defineArticle,
+  defineOrganization,
+  definePerson,
+  useResourcePageMeta,
+  useRuntimeConfig,
+  useSchemaOrg,
+  useSite,
+} from '#imports'
+import type { ResourcePageContext } from '#build/storipress-urls.mjs'
+import urls from '#build/storipress-urls.mjs'
+
+type ArticleSchema = ReturnType<typeof defineArticle>
+export const schemaOrgHooks = new Hookable<{
+  'karbon:article-schema': (schema: ArticleSchema) => void
+}>()
 
 export function useArticleSchemaOrg() {
   const pageMeta = useResourcePageMeta()
   const site = useSite()
 
   if (pageMeta.value) {
-    useSchemaOrg([
-      defineOrganization({
-        name: () => site.value?.name || '',
-        logo: () => site.value?.logo?.url,
-      }),
-      getDefineArticle(pageMeta.value, site),
-    ])
+    const articleSchema = getDefineArticle(pageMeta.value, site)
+    tryOnServer(async () => {
+      await schemaOrgHooks.callHookParallel('karbon:article-schema', articleSchema)
+      useSchemaOrg([
+        defineOrganization({
+          name: () => site.value?.name || '',
+          logo: () => site.value?.logo?.url,
+        }),
+        articleSchema,
+      ])
+    })
+  }
+}
+
+function tryOnServer(fn: () => Promise<void>) {
+  if (process.server) {
+    onServerPrefetch(fn)
+  } else {
+    fn()
   }
 }
 
 type PageMeta = NonNullable<ReturnType<typeof useResourcePageMeta>['value']>
+const invalidContext = { identity: 'invalid', prefix: '', resource: 'invalid' } as unknown as ResourcePageContext
 
 function getDefineArticle(pageMeta: PageMeta, site: ReturnType<typeof useSite>) {
+  const runtimeConfig = useRuntimeConfig()
+  const siteUrl = withoutTrailingSlash(runtimeConfig.public.siteUrl as string)
   const article: Article = pageMeta.meta
   const authors = article.authors.map((author) => {
     const { first_name, last_name, full_name } = author
+    const hasAuthorPage = urls.author.enable
+    const url = hasAuthorPage
+      ? resolveURL(siteUrl, urls.author.toURL(author, urls.author._context ?? invalidContext))
+      : undefined
+    const socials = Object.values(author.socials as Record<string, string>).map((url) => withHttps(url))
 
     return definePerson({
       familyName: last_name,
       givenName: first_name,
       name: full_name,
+      url,
+      sameAs: [url, ...socials].filter((x): x is string => Boolean(x)),
     })
   })
   const doc = parse(article.html || '')
@@ -44,20 +83,18 @@ function getDefineArticle(pageMeta: PageMeta, site: ReturnType<typeof useSite>) 
   return defineArticle({
     '@context': 'https://schema.org',
     '@type': 'Article',
-    url: pageMeta.route,
+    url: resolveURL(siteUrl, pageMeta.route),
     publisher: () =>
       defineOrganization({
         name: () => site.value?.name || '',
         logo: () => site.value?.logo?.url,
       }),
     headline: article.title,
-    mainEntityOfPage: {
-      '@type': 'Article',
-      '@id': pageMeta.route,
-    },
+    mainEntityOfPage: resolveURL(siteUrl, pageMeta.route),
     articleBody: article.plaintext,
-    authors,
+    author: authors,
     image,
     datePublished: article.published_at,
+    dateModified: article.updated_at || '',
   })
 }
--- packages/karbon/src/runtime/types.ts
+++ packages/karbon/src/runtime/types.ts
@@ -205,6 +205,7 @@ export interface Article {
   id: string
   blurb?: string | null
   published_at?: any | null
+  updated_at?: string | null
   slug: string
   title: string
   featured: boolean
