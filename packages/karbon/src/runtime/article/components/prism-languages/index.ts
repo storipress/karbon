944e8db477ba1e8c37d19cddbf10d9ed02601065
DanSnow <dododavid006@gmail.com>
2023-09-21 15:19:55 +0800

    fix: fix code highlight esm compatible

--- packages/karbon/src/runtime/article/components/prism-languages/index.ts
+++ packages/karbon/src/runtime/article/components/prism-languages/index.ts
@@ -1,12 +1,1 @@
-import Prism from 'prismjs/components/prism-core'
-import 'prismjs/components/prism-clike'
-import 'prismjs/components/prism-markup'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-css'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-markup-templating'
-import 'prismjs/components/prism-php'
-import 'prismjs/components/prism-python'
-import 'prismjs/themes/prism.css'
-
-export default Prism
+export { default } from '@storipress/code-highlight'
