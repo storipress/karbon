659f361258b1166f1fa73805bb064806c20b7a28
DanSnow <dododavid006@gmail.com>
2024-04-02 17:12:00 +0800

    style: format with prettier]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,15 @@
 import antfu from '@antfu/eslint-config'
+import prettier from 'eslint-plugin-prettier'
 
 export default antfu({
   stylistic: false,
   vue: true,
+  plugins: { prettier },
   rules: {
     'node/prefer-global/process': 'off',
     'vue/singleline-html-element-content-newline': 'off',
     'vue/html-self-closing': 'off',
+
+    'prettier/prettier': 'error',
   },
 })
