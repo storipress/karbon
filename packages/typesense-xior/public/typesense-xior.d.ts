e5cbcaaf0c9cccda96abf6c98dda415edb673b77
DanSnow <dododavid006@gmail.com>
2024-04-11 18:29:31 +0800

    fix: upgrade typesense + expose more type

--- packages/typesense-xior/public/typesense-xior.d.ts
+++ packages/typesense-xior/public/typesense-xior.d.ts
@@ -1,2 +1,4 @@
 export * from 'typesense'
 export { default } from 'typesense'
+export type { SearchParams, SearchParamsWithPreset, SearchResponse } from 'typesense/src/Typesense/Documents'
+export type { MultiSearchResponse } from 'typesense/src/Typesense/MultiSearch'
