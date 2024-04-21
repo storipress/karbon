d7488211274af79e57bb1d63a72b98a88d3d9107
DanSnow <dododavid006@gmail.com>
2024-04-21 23:22:43 +0800

    refactor: use utils package

--- packages/karbon/src/runtime/composables/article-filter.ts
+++ packages/karbon/src/runtime/composables/article-filter.ts
@@ -1,20 +1,7 @@
-import { useNuxtApp } from '#imports'
+import { htmlFilter } from '@storipress/karbon-utils'
+
+export { filterHTMLTag } from '@storipress/karbon-utils'
 
 export function useArticleFilter() {
-  const { $entities } = useNuxtApp()
-
-  return (html: string): string => {
-    if (!html) {
-      return ''
-    }
-    return $entities.decode(filterHTMLTag(html))
-  }
-}
-
-export function filterHTMLTag(text: string) {
-  if (!text) {
-    return ''
-  }
-
-  return text.replace(/<\/?[^>]*>/g, '').trim()
+  return htmlFilter
 }
