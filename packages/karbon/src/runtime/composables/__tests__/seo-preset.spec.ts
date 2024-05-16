c99d73d90145f351f3c62844f6e84a797e0cbc70
DanSnow <dododavid006@gmail.com>
2024-05-16 17:06:56 +0800

    test: add test

--- packages/karbon/src/runtime/composables/__tests__/seo-preset.spec.ts
+++ packages/karbon/src/runtime/composables/__tests__/seo-preset.spec.ts
@@ -1,5 +1,6 @@
 import { expect, it } from 'vitest'
 import { identity, reduce } from 'remeda'
+import { htmlFilter } from '@storipress/karbon-utils'
 import type { SEOContext } from '../seo-preset'
 import { resolveSEOPresets } from '../seo-preset'
 import type { BaseMeta, ResourcePage } from '../../types'
@@ -47,6 +48,76 @@ it('can handle article', () => {
     title: 'title',
     blurb: 'blurb',
     bio: 'author_bio',
+    cover: {
+      url: 'cover_url',
+    },
+    author: {
+      name: 'author_name',
+    },
+  }
+
+  for (const handler of handlers) {
+    handler(article, context)
+  }
+
+  expect(reduce(results, (acc, cur) => Object.assign(acc, cur), {})).toMatchSnapshot()
+})
+
+it.only('can handle article with html', () => {
+  const handlers = resolveSEOPresets([{ preset: 'basic' }])
+  const results: unknown[] = []
+  const resourceURL: ResourcePage<BaseMeta> = {
+    enable: true,
+    isValid: () => true,
+    getIdentity: () => ({ type: 'article', id: '1' }),
+    toURL: () => '',
+    route: '',
+  }
+  const context: SEOContext = {
+    metaType: 'article',
+    runtimeConfig: {} as any,
+    site: {
+      name: 'site_name',
+    },
+    articleFilter: htmlFilter,
+    useHead: (input: unknown) => void results.push(input),
+    useSeoMeta: (input: unknown) => void results.push(input),
+    resourceUrls: {
+      article: {
+        ...resourceURL,
+        toURL: () => 'article_url',
+      },
+      desk: {
+        ...resourceURL,
+        toURL: () => 'desk_url',
+      },
+      author: {
+        ...resourceURL,
+        toURL: () => 'author_url',
+      },
+      tag: {
+        ...resourceURL,
+        toURL: () => 'tag_url',
+      },
+    },
+  }
+
+  const article = {
+    title: '<p>title</p>',
+    blurb: 'blurb',
+    bio: 'author_bio',
+    seo: {
+      og: {
+        title: '',
+        description: '',
+      },
+      meta: {
+        title: '',
+        description: '',
+      },
+      hasSlug: false,
+      ogImage: '',
+    },
     cover: {
       url: 'cover_url',
     },
