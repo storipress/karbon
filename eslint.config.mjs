2c15cbf93792e7ee19b3036402fd19772f98c78e
DanSnow <dododavid006@gmail.com>
2024-06-09 17:18:04 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,20 @@
 import antfu from '@antfu/eslint-config'
 import prettier from 'eslint-plugin-prettier'
 
-export default antfu({
-  stylistic: false,
-  vue: true,
-  plugins: { prettier },
-  rules: {
-    'node/prefer-global/process': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/html-self-closing': 'off',
+export default antfu(
+  {
+    stylistic: false,
+    vue: true,
+    plugins: { prettier },
+    rules: {
+      'node/prefer-global/process': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/html-self-closing': 'off',
 
-    'prettier/prettier': 'error',
+      'prettier/prettier': 'error',
+    },
   },
-})
+  {
+    ignores: ['**/dist/**', 'packages/karbon/bin/**', '**/*.md', '**/*.toml', '.moon/templates/**'],
+  },
+)
--- packages/karbon/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  rules: {
-    'n/prefer-global/process': 'off',
-  },
-}
